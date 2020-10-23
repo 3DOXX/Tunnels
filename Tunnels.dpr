aff59e78d221e45971821986472883b3a74b618b
3DOXX <33119568+3DOXX@users.noreply.github.com>
2020-10-23 21:49:37 +0200

    Project files updated

--- Tunnels.dpr
+++ Tunnels.dpr
@@ -1,15 +1,31 @@
 program Tunnels;
 { *************************************************************************** }
-{$APPTYPE CONSOLE}
+{$APPTYPE GUI}
 {$R *.res}
 { *************************************************************************** }
 uses
-  System.SysUtils;
+  SDL2,
+  System.SysUtils,
+  App.Activity in 'App\App.Activity.pas',
+  App in 'App\App.pas',
+  App.Console in 'App\App.Console.pas',
+  App.EventHandler in 'App\App.EventHandler.pas',
+  App.Window in 'App\App.Window.pas';
+
+{ *************************************************************************** }
+var
+  FApp : TApp;
 { *************************************************************************** }
 begin
   try
+    FApp := TApp.Create;
+    FApp.EventHandler.HandleEvents;
+    FreeAndNil(FApp);
   except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+    on E : Exception do
+      SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                               PAnsiChar(AnsiString(E.ClassName)),
+                               PAnsiChar(AnsiString(E.Message)),
+                               nil);
   end;
 end.
