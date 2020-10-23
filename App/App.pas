6ea9cf2ef657c9258e8b6a8b118bcbdbe20ffd1e
3DOXX <33119568+3DOXX@users.noreply.github.com>
2020-10-23 19:52:05 +0200

    SDL2 added

--- App/App.pas
+++ App/App.pas
@@ -9,6 +9,7 @@ type
   TApp = class
     private
       FConsole : TConsole;
+      function InitSDL : boolean;
     public
       constructor Create;
       destructor  Destroy; override;
@@ -17,19 +18,39 @@ type
 implementation
 { *************************************************************************** }
 uses
-  SysUtils;
+  SysUtils,
+  SDL2;
 { *************************************************************************** }
 { PUBLIC                                                                      }
 { *************************************************************************** }
 constructor TApp.Create;
 begin
   FConsole := TConsole.Create;
+  if InitSDL then
+  begin
+    // ...
+  end
+  else
+    raise Exception.Create(string(SDL_GetError));
 end;
 { *************************************************************************** }
 destructor TApp.Destroy;
 begin
   FreeAndNil(FConsole);
+  SDL_Quit;
   inherited;
+end;
+{ *************************************************************************** }
+{ PRIVATE                                                                     }
+{ *************************************************************************** }
+function TApp.InitSDL : boolean;
+begin
+  FConsole.LogMessage('Initializing SDL2...', False);
+  Result := (SDL_Init(SDL_INIT_VIDEO) = 0);
+  if Result then
+    FConsole.LogSuccess
+  else
+    FConsole.LogError(string(SDL_GetError));
 end;
 { *************************************************************************** }
 end.
