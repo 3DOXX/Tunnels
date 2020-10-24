c8d184d11a5237bd99cd50b23726b5ebe73bb025
3DOXX <33119568+3DOXX@users.noreply.github.com>
2020-10-24 22:59:05 +0200

    VertexBuffer + VertexArray added

---
+++ Graphics/Graphics.VertexArray.pas
@@ -0,0 +1,54 @@
+unit Graphics.VertexArray;
+{ *************************************************************************** }
+interface
+{ *************************************************************************** }
+uses
+  dglOpenGl,
+  Graphics.VertexBuffer;
+{ *************************************************************************** }
+type
+  TVertexArray = class
+    private
+      FID         : GlUint;
+      FAttributes : Integer;
+    public
+      constructor Create;
+      destructor  Destroy; override;
+
+      procedure Activate;
+      procedure AssignBuffer(const _Buffer : TVertexBuffer;
+                             const _Size   : GlInt = 3;
+                             const _Type   : GlEnum  = GL_FLOAT);
+  end;
+{ *************************************************************************** }
+implementation
+{ *************************************************************************** }
+{ PUBLIC                                                                      }
+{ *************************************************************************** }
+procedure TVertexArray.Activate;
+begin
+  glBindVertexArray(FID);
+end;
+{ *************************************************************************** }
+procedure TVertexArray.AssignBuffer(const _Buffer : TVertexBuffer;
+                                    const _Size   : GlInt;
+                                    const _Type   : GlEnum);
+begin
+  Activate;
+  _Buffer.Activate;
+  glVertexAttribPointer(FAttributes, _Size, _Type, GL_FALSE, 0, nil);
+  inc(FAttributes);
+end;
+{ *************************************************************************** }
+constructor TVertexArray.Create;
+begin
+  glGenVertexArrays(1, @FID);
+end;
+{ *************************************************************************** }
+destructor TVertexArray.Destroy;
+begin
+  glDeleteVertexArrays(1, @FID);
+  inherited;
+end;
+{ *************************************************************************** }
+end.
---
+++ Graphics/Graphics.VertexBuffer.pas
@@ -0,0 +1,48 @@
+unit Graphics.VertexBuffer;
+{ *************************************************************************** }
+interface
+{ *************************************************************************** }
+uses
+  dglOpenGl;
+{ *************************************************************************** }
+type
+  TVertexData = array of Single;
+{ *************************************************************************** }
+  TVertexBuffer = class
+    private
+      FID : GlUint;
+    public
+      constructor Create;
+      destructor  Destroy; override;
+
+      procedure Activate;
+      procedure AssignData(const _Data : TVertexData);
+  end;
+{ *************************************************************************** }
+implementation
+{ *************************************************************************** }
+{ PUBLIC                                                                      }
+{ *************************************************************************** }
+procedure TVertexBuffer.Activate;
+begin
+  glBindBuffer(GL_ARRAY_BUFFER, FID);
+end;
+{ *************************************************************************** }
+constructor TVertexBuffer.Create;
+begin
+  glGenBuffers(1, @FID);
+end;
+{ *************************************************************************** }
+destructor TVertexBuffer.Destroy;
+begin
+  glDeleteBuffers(1, @FID);
+  inherited;
+end;
+{ *************************************************************************** }
+procedure TVertexBuffer.AssignData(const _Data : TVertexData);
+begin
+  Activate;
+  glBufferData(GL_ARRAY_BUFFER, sizeof(_Data), @_Data, GL_STATIC_DRAW);
+end;
+{ *************************************************************************** }
+end.
